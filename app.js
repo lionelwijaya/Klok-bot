e20b652b5b9a7031a3253bd1727f7b37493b53dc
lionelwijaya <63404416+lionelwijaya@users.noreply.github.com>
2022-01-14 22:34:59 +0700

    Deleted token for safety

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 require("dotenv").config();
-const TOKEN = "OTMxNDcxMzIwODYzMDgwNTA5.YeE6RA.EuT3Y0BL9aTk9fqGTcGXaQ-OCKA";
 
 const prefix = "?";
 const client = new Discord.Client({
